15c872022d2a6643568439ec40594f688b67e77e
dazaid <dazaink.dzk@gmail.com>
2024-11-14 12:15:51 +0000

    no matter

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+
+const Home = () => {
+  return (
+ 
+   <Box className="bg-[url('./public/assets/HomeBkg.png')] bg-cover h-screen">
+      <h1 className="text-white">Bienvenido al Home</h1>
+    </Box>
+  );
+};
+
+export default Home;
