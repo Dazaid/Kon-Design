df96a9b1743ca5bce0695125ee0518ddbd10979c
dazaid <dazaink.dzk@gmail.com>
2024-11-19 14:46:14 +0000

    Updates

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Asegúrate de que incluya tus archivos
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
